3d60e3c345d9f76679e9813fffd14f5c00587f49
Layzerg11 <vspishka17@mail.ru>
2023-10-04 21:01:56 +0400

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,53 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import ru.kata.spring.boot_security.demo.services.UserServiceImp;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    UserRepository userRepository;
+    private final UserServiceImp userServiceImp;
+
+    @Autowired
+    public AdminController(UserServiceImp userServiceImp) {
+        this.userServiceImp = userServiceImp;
+    }
+
+    @GetMapping("/{id}")
+    public String user(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userServiceImp.findUserById(id));
+        return "edit";
+    }
+    @GetMapping
+    public String pageForAdmin(Model model, @ModelAttribute("user") User user) {
+        model.addAttribute("users", userServiceImp.getUserList());
+        return "admin";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("admin") User user) {
+        userServiceImp.createUser(user);
+        return "redirect:/admin";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("admin") User user, @PathVariable("id") long id) {
+        userServiceImp.updateUser(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping
+    public String delete(@ModelAttribute("user") User user) {
+        userServiceImp.deleteUser(user.getId());
+        return "redirect:/admin";
+    }
+
+}
