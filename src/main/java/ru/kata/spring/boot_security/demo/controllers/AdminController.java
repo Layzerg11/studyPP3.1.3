8d92efab9dca7a80a46af7fe6829ce0a3d1837aa
Layzerg11 <vspishka17@mail.ru>
2023-10-07 13:00:35 +0400

    version#3

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -5,45 +5,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.UserServiceImp;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final UserServiceImp userServiceImp;
+    private final UserService userService;
 
     @Autowired
-    public AdminController(UserServiceImp userServiceImp) {
-        this.userServiceImp = userServiceImp;
+    public AdminController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/{id}")
     public String user(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userServiceImp.findUserById(id));
+        model.addAttribute("user", userService.findUserById(id));
         return "edit";
     }
     @GetMapping
     public String pageForAdmin(Model model, @ModelAttribute("user") User user) {
-        model.addAttribute("users", userServiceImp.getUserList());
+        model.addAttribute("users", userService.getUserList());
         return "admin";
     }
 
     @PostMapping
     public String create(@ModelAttribute("admin") User user) {
-        userServiceImp.createUser(user);
+        userService.createUser(user);
         return "redirect:/admin";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("admin") User user) {
-        userServiceImp.updateUser(user);
+        userService.updateUser(user);
         return "redirect:/admin";
     }
 
     @DeleteMapping
     public String delete(@ModelAttribute("user") User user) {
-        userServiceImp.deleteUser(user.getId());
+        userService.deleteUser(user.getId());
         return "redirect:/admin";
     }
 
